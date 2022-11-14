2e3e09de4882803dd0c6e50b7a77b3e4a1b72cd5
Van Dinh <tvdinhit@gmail.com>
2022-11-14 09:07:49 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+name = ''
+
+def say_hi(name):
+	print(f'Hi {name}')
+
+
+
+def say_hello(name):
+	print(f'Hello {name}')
+
+
+
+def say_hiya(name):
+	print(f'Hiya {name}')
+
+
+for func in (say_hi, say_hello, say_hiya):
+	func(name)
